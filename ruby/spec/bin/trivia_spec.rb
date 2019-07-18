50adeca1826c63601eadae604c32c724628e8b68
Kevin Burleigh <klb@kindling>
2019-07-18 14:22:12 -0500

    add basic acceptance tests; get random seed under control

---
+++ ruby/spec/bin/trivia_spec.rb
@@ -0,0 +1,30 @@
+require 'byebug'
+
+RSpec.describe '/bin/trivia' do
+  context 'when the random seed is 1234' do
+    let(:seed) { 1234 }
+    let(:golden_output) {
+      File.read("./spec/bin/golden_output_#{seed}.txt")
+    }
+
+    it 'outputs the correct stuff' do
+      ENV['TRIVIA_RANDOM_SEED'] = seed.to_s
+      output = `./bin/trivia`
+      expect(output).to eq(golden_output)
+    end
+  end
+
+  context 'when the random seed is 4321' do
+    let(:seed) { 4321 }
+    let(:golden_output) {
+      File.read("./spec/bin/golden_output_#{seed}.txt")
+    }
+
+    it 'outputs the correct stuff' do
+      ENV['TRIVIA_RANDOM_SEED'] = seed.to_s
+      output = `./bin/trivia`
+      expect(output).to eq(golden_output)
+    end
+  end
+end
+
