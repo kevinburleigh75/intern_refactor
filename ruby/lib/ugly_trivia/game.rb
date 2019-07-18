7b1b7ac9754b90132689f9c11defc5bcb9dae319
Kevin Burleigh <klb@kindling>
2019-07-18 16:09:11 -0500

    refactored correct/incorrect answer

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -2,48 +2,36 @@ module UglyTrivia
   class Game
     def  initialize
       @players = []
-      @places = Array.new(6, 0)
-      @purses = Array.new(6, 0)
+      @places         = Array.new(6, 0)
+      @purses         = Array.new(6, 0)
       @in_penalty_box = Array.new(6, nil)
 
-      @pop_questions = []
+      @pop_questions     = []
       @science_questions = []
-      @sports_questions = []
-      @rock_questions = []
+      @sports_questions  = []
+      @rock_questions    = []
 
       @current_player = 0
       @is_getting_out_of_penalty_box = false
 
       50.times do |i|
-        @pop_questions.push "Pop Question #{i}"
+        @pop_questions.push     "Pop Question #{i}"
         @science_questions.push "Science Question #{i}"
-        @sports_questions.push "Sports Question #{i}"
-        @rock_questions.push create_rock_question(i)
+        @sports_questions.push  "Sports Question #{i}"
+        @rock_questions.push    "Rock Question #{i}"
       end
-    end
-
-    def create_rock_question(index)
-      "Rock Question #{index}"
-    end
-
-    def is_playable?
-      how_many_players >= 2
     end
 
     def add(player_name)
       @players.push player_name
-      @places[how_many_players] = 0
-      @purses[how_many_players] = 0
-      @in_penalty_box[how_many_players] = false
+      @places[@players.length] = 0
+      @purses[@players.length] = 0
+      @in_penalty_box[@players.length] = false
 
       puts "#{player_name} was added"
       puts "They are player number #{@players.length}"
 
       true
-    end
-
-    def how_many_players
-      @players.length
     end
 
     def roll(roll)
@@ -65,9 +53,7 @@ module UglyTrivia
           puts "#{@players[@current_player]} is not getting out of the penalty box"
           @is_getting_out_of_penalty_box = false
           end
-
       else
-
         @places[@current_player] = @places[@current_player] + roll
         @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
 
@@ -77,78 +63,72 @@ module UglyTrivia
       end
     end
 
-  private
-
-    def ask_question
-      puts @pop_questions.shift if current_category == 'Pop'
-      puts @science_questions.shift if current_category == 'Science'
-      puts @sports_questions.shift if current_category == 'Sports'
-      puts @rock_questions.shift if current_category == 'Rock'
-    end
-
-    def current_category
-      return 'Pop' if @places[@current_player] == 0
-      return 'Pop' if @places[@current_player] == 4
-      return 'Pop' if @places[@current_player] == 8
-      return 'Science' if @places[@current_player] == 1
-      return 'Science' if @places[@current_player] == 5
-      return 'Science' if @places[@current_player] == 9
-      return 'Sports' if @places[@current_player] == 2
-      return 'Sports' if @places[@current_player] == 6
-      return 'Sports' if @places[@current_player] == 10
-      return 'Rock'
-    end
-
-  public
-
     def was_correctly_answered
       if @in_penalty_box[@current_player]
         if @is_getting_out_of_penalty_box
-          puts 'Answer was correct!!!!'
-          @purses[@current_player] += 1
-          puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
-
-          winner = did_player_win()
-          @current_player += 1
-          @current_player = 0 if @current_player == @players.length
-
-          winner
-        else
-          @current_player += 1
-          @current_player = 0 if @current_player == @players.length
-          true
+          do_correct_answer(output_typo: false)
         end
-
-
-
       else
-
-        puts "Answer was corrent!!!!"
-        @purses[@current_player] += 1
-        puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
-
-        winner = did_player_win
-        @current_player += 1
-        @current_player = 0 if @current_player == @players.length
-
-        return winner
+        do_correct_answer(output_typo: true)
       end
+
+      game_should_continue = !current_player_is_a_winner?
+      return game_should_continue
     end
 
-    def wrong_answer
-  		puts 'Question was incorrectly answered'
-  		puts "#{@players[@current_player]} was sent to the penalty box"
-  		@in_penalty_box[@current_player] = true
+    def was_incorrectly_answered
+      puts 'Question was incorrectly answered'
+      puts "#{@players[@current_player]} was sent to the penalty box"
+      @in_penalty_box[@current_player] = true
 
+      game_should_continue = true
+      return game_should_continue
+    end
+
+    def advance_to_next_player
       @current_player += 1
       @current_player = 0 if @current_player == @players.length
-  		return true
     end
 
   private
 
-    def did_player_win
-      !(@purses[@current_player] == 6)
+    def do_correct_answer(output_typo:)
+      if output_typo
+        puts 'Answer was corrent!!!!'
+      else
+        puts 'Answer was correct!!!!'
+      end
+
+      @purses[@current_player] += 1
+      puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
+    end
+
+    def ask_question
+      puts @pop_questions.shift     if current_category == 'Pop'
+      puts @science_questions.shift if current_category == 'Science'
+      puts @sports_questions.shift  if current_category == 'Sports'
+      puts @rock_questions.shift    if current_category == 'Rock'
+    end
+
+    def current_category
+      return 'Pop'     if @places[@current_player] == 0
+      return 'Pop'     if @places[@current_player] == 4
+      return 'Pop'     if @places[@current_player] == 8
+      return 'Science' if @places[@current_player] == 1
+      return 'Science' if @places[@current_player] == 5
+      return 'Science' if @places[@current_player] == 9
+      return 'Sports'  if @places[@current_player] == 2
+      return 'Sports'  if @places[@current_player] == 6
+      return 'Sports'  if @places[@current_player] == 10
+      return 'Rock'
+    end
+
+    # def is_playable?  ## this unused?
+    #   how_many_players >= 2
+    # end
+
+    def current_player_is_a_winner?
+      @purses[@current_player] == 6
     end
   end
 end
--- ruby/spec/bin/trivia_spec.rb
+++ ruby/spec/bin/trivia_spec.rb
@@ -26,5 +26,31 @@ RSpec.describe '/bin/trivia' do
       expect(output).to eq(golden_output)
     end
   end
+
+  context 'when the random seed is 1111' do
+    let(:seed) { 1111 }
+    let(:golden_output) {
+      File.read("./spec/bin/golden_output_#{seed}.txt")
+    }
+
+    it 'outputs the correct stuff' do
+      ENV['TRIVIA_RANDOM_SEED'] = seed.to_s
+      output = `./bin/trivia`
+      expect(output).to eq(golden_output)
+    end
+  end
+
+  context 'when the random seed is 2222' do
+    let(:seed) { 2222 }
+    let(:golden_output) {
+      File.read("./spec/bin/golden_output_#{seed}.txt")
+    }
+
+    it 'outputs the correct stuff' do
+      ENV['TRIVIA_RANDOM_SEED'] = seed.to_s
+      output = `./bin/trivia`
+      expect(output).to eq(golden_output)
+    end
+  end
 end
 
